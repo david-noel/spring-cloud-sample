9098f93548b6ff8c3575b7fc3f07b1ecd59d25e2
Dave Noel <dave@devbury.com>
2015-03-30 22:44:27 -0400

    update to snapshot build and add hystrix circuitbreaker

--- service2/src/main/java/cloud/service2/Application.java
+++ service2/src/main/java/cloud/service2/Application.java
@@ -1,5 +1,6 @@
 package cloud.service2;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableConfigurationProperties(Service2Properties.class)
+@EnableHystrix
 @RestController
 public class Application {
 
@@ -40,8 +43,17 @@ public class Application {
 
         discoveryClient.getInstances("service1")
                 .forEach(si -> logger.warn("service1 instance {}:{}", si.getHost(), si.getPort()));
+        return welcomeMessage(name) + service2Properties.getBody();
+    }
+
+    @HystrixCommand(fallbackMethod = "defaultWelcomeMessage2")
+    public String welcomeMessage(String name) {
         //return service1.welcome(name) + service2Properties.getBody();
-        return restTemplate.getForObject("http://service1/welcome/{name}", String.class, name) + service2Properties.getBody();
+        return restTemplate.getForObject("http://service1/welcome/{name}", String.class, name);
+    }
+
+    public String defaultWelcomeMessage(String name) {
+        return "Welcome " + name;
     }
 
     public static void main(String... args) {
