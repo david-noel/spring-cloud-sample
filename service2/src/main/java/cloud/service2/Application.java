f43b79ffec85c82a04d903c6a109e20635241c35
Dave Noel <dnoel@thunderhead.com>
2016-08-09 15:58:17 -0400

    update to latest version of boot.  remove rabbit bus stuff

--- service2/src/main/java/cloud/service2/Application.java
+++ service2/src/main/java/cloud/service2/Application.java
@@ -9,8 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,16 +29,22 @@ public class Application {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     @Autowired
-    private Service1 service1;
+    private RestTemplate restTemplate;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private Service1 service1;
 
     @Autowired
     private DiscoveryClient discoveryClient;
 
     @Autowired
     private Service2Properties service2Properties;
+
+	@Bean
+	@LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     @RequestMapping("/body/{name}")
     public String body(@PathVariable String name) {
@@ -46,10 +54,13 @@ public class Application {
         return welcomeMessage(name) + service2Properties.getBody();
     }
 
-    @HystrixCommand(fallbackMethod = "defaultWelcomeMessage2")
+    @HystrixCommand(fallbackMethod = "defaultWelcomeMessage")
     public String welcomeMessage(String name) {
-        //return service1.welcome(name) + service2Properties.getBody();
-        return restTemplate.getForObject("http://service1/welcome/{name}", String.class, name);
+        String message1 = service1.welcome(name);
+        // same thing
+        String message2 = restTemplate.getForObject("http://service1/welcome/{name}", String.class, name);
+        assert message1.equals(message2);
+        return message1;
     }
 
     public String defaultWelcomeMessage(String name) {
